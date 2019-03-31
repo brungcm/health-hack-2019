5f86546ffeb6ce33b28ad2b7c602bd9095bc67f3
Mona Lisa <ltung@ciandt.com>
2019-03-30 22:08:45 -0300

    Added tracker app api

---
+++ api/README.md
@@ -0,0 +1,11 @@
+# Tracker app API
+
+### Build docker image
+```bash
+docker build -t health-hack-2019/tracker-api .
+```
+
+### Run docker image
+```bash
+docker run -d -v ${PWD}/../:/data -p 5000:5000 health-hack-2019/tracker-api
+```
---
+++ api/main.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+import json
+import logging
+import os
+
+from flask import Flask, jsonify
+
+logger = logging.getLogger(__name__)
+logging.getLogger().setLevel(logging.INFO)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def default():
+    return 'People-Tracker-App OK'
+
+
+@app.route('/status')
+def get_status():
+    status_data = {}
+    filepath = os.environ['STATUS_FILE']
+    with open(filepath, 'r') as status_file:
+        status_data = json.load(status_file)
+    return jsonify(status_data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
